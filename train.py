3c6fc34b81714e402c6433713aad9153c0bb7613
AhmedImtiazPrio <ahmed.imtiaz.prio@gmail.com>
2018-03-11 19:10:39 +0600

    added tensorboard callback, fixed issues with modelcheckpoint

--- train.py
+++ train.py
@@ -11,21 +11,22 @@ from tensorflow import set_random_seed
 set_random_seed(1)
 from keras.utils import to_categorical
 from keras.optimizers import Adam, SGD
-from keras.callbacks import CSVLogger, ModelCheckpoint
+from keras.callbacks import CSVLogger, ModelCheckpoint, TensorBoard
 from keras.utils import plot_model
 from keras.layers import Dense, Dropout
 from keras.models import Model
-from sklearn.metrics import recall_score
+from sklearn.metrics import recall_score, confusion_matrix
 import pandas as pd
+import os
 import tables
 from datetime import datetime
 import argparse
 
-def heartnet_transfer(load_path='/media/taufiq/Data/heart_sound/interspeech_compare/weights.0148-0.8902.hdf5',lr=0.0012843784,lr_decay=0.0001132885,num_dense=20,trainable=False):
+def heartnet_transfer(load_path='/media/taufiq/Data/heart_sound/interspeech_compare/weights.0148-0.8902.hdf5',lr=0.0012843784,lr_decay=0.0001132885,num_dense=20,trainable=False,dropout_rate=0.):
     model = heartnet(load_path=load_path,FIR_train=False,trainable=trainable)
     x = model.layers[-4].output
     x = Dense(num_dense,activation='relu') (x)
-    x = Dropout(rate=0.5,seed=1) (x)
+    x = Dropout(rate=dropout_rate,seed=1) (x)
     output = Dense(3,activation='softmax')(x)
     model = Model(inputs=model.input,outputs=output)
     sgd = SGD(lr=lr)
@@ -39,14 +40,20 @@ if __name__ == '__main__':
     model_dir = '/media/taufiq/Data/heart_sound/models/'
     log_name = foldname + ' ' + str(datetime.now())
     checkpoint_name = model_dir + log_name + "/" + 'weights.{epoch:04d}-{val_acc:.4f}.hdf5'
+    if not os.path.exists(model_dir + log_name):
+        os.makedirs(model_dir + log_name)
+    log_dir = '/media/taufiq/Data/heart_sound/interspeech_compare/logs/'
+
     ##### Load Model ######
 
     load_path='/media/taufiq/Data/heart_sound/interspeech_compare/weights.0148-0.8902.hdf5'
     lr = 0.00001
     num_dense = 20
-    epochs = 100
+    epochs = 250
+    batch_size = 128
+    dropout_rate = 0.
     trainable = True
-    model = heartnet_transfer(load_path=load_path,lr=lr,num_dense=num_dense,trainable=trainable)
+    model = heartnet_transfer(load_path=load_path,lr=lr,num_dense=num_dense,trainable=trainable,dropout_rate=dropout_rate)
     plot_model(model,"model.png",show_layer_names=True,show_shapes=True)
 
     ##### Load Data ######
@@ -69,14 +76,22 @@ if __name__ == '__main__':
     csv_logger = CSVLogger('training.csv')
     modelcheckpnt = ModelCheckpoint(filepath=checkpoint_name,
                                     monitor='val_acc', save_best_only=True, mode='max')
+    tensbd = TensorBoard(log_dir=log_dir + log_name,
+                         batch_size=batch_size,
+                         histogram_freq=100,
+                         # embeddings_freq=99,
+                         # embeddings_layer_names=embedding_layer_names,
+                         # embeddings_data=x_val,
+                         # embeddings_metadata=metadata_file,
+                         write_images=False)
 
     ### Train ###
     model.fit(x_train,y_train,
-              batch_size=128,
+              batch_size=batch_size,
               epochs=epochs,
               verbose=2,
               shuffle=True,
-              callbacks=[ModelCheckpoint, csv_logger],
+              callbacks=[modelcheckpnt, csv_logger],
               validation_data=(x_val,y_val))
 
     res_thresh =.5
@@ -105,5 +120,6 @@ if __name__ == '__main__':
             true.append(0)
 
         start_idx = start_idx + int(s)
-    score = recall_score(true, pred, average='macro')
+    score = recall_score(y_pred=pred, y_true=true, average='macro')
     print(score)
+    print(confusion_matrix(y_true=true,y_pred=pred))
