e0b2c822cd19cab85d97610aa9eeb60962b00adb
AhmedImtiazPrio <ahmed.imtiaz.prio@gmail.com>
2018-03-11 23:47:01 +0600

    major result generation bug

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from tensorflow import set_random_seed
 set_random_seed(1)
 from keras.utils import to_categorical
 from keras.optimizers import Adam, SGD
-from keras.callbacks import CSVLogger, ModelCheckpoint, TensorBoard
+from keras.callbacks import CSVLogger, ModelCheckpoint, TensorBoard, Callback
 from keras.utils import plot_model
 from keras.layers import Dense, Dropout
 from keras.models import Model
@@ -33,6 +33,51 @@ def heartnet_transfer(load_path='/media/taufiq/Data/heart_sound/interspeech_comp
     model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
+class log_UAR(Callback):
+
+    def __init__(self, x_val, y_val, val_parts):
+        self.x_val = x_val
+        self.y_val = y_val
+        self.val_parts = val_parts
+
+
+    def on_epoch_end(self, epoch, logs):
+        if logs is not None:
+            y_pred = self.model.predict(self.x_val, verbose=0)
+            y_pred = np.argmax(y_pred, axis=-1)
+            self.y_val_ = np.transpose(np.argmax(self.y_val, axis=-1))
+            true = []
+            pred = []
+            start_idx = 0
+            for s in self.val_parts:
+
+                if not s:  ## for e00032 in validation0 there was no cardiac cycle
+                    continue
+                # ~ print "part {} start {} stop {}".format(s,start_idx,start_idx+int(s)-1)
+
+                temp_ = self.y_val_[start_idx:start_idx + int(s) - 1]
+                temp = y_pred[start_idx:start_idx + int(s) - 1]
+
+                if (sum(temp == 0) > sum(temp == 1)) and (sum(temp == 0) > sum(temp == 2)):
+                    pred.append(0)
+                elif (sum(temp == 2) > sum(temp == 1)) and (sum(temp == 2) > sum(temp == 0)):
+                    pred.append(2)
+                else:
+                    pred.append(1)
+
+                if (sum(temp_ == 0) > sum(temp_ == 1)) and (sum(temp_ == 0) > sum(temp_ == 2)):
+                    true.append(0)
+                elif (sum(temp_ == 2) > sum(temp_ == 1)) and (sum(temp_ == 2) > sum(temp_ == 0)):
+                    true.append(2)
+                else:
+                    true.append(1)
+
+                start_idx = start_idx + int(s)
+
+            score = recall_score(y_pred=pred, y_true=true, average='macro')
+            logs['UAR'] = np.array(score)
+
+
 if __name__ == '__main__':
 
     fold_dir = '/media/taufiq/Data/heart_sound/interspeech_compare/feature/segmented_noFIR/'
@@ -49,14 +94,15 @@ if __name__ == '__main__':
     load_path='/media/taufiq/Data/heart_sound/interspeech_compare/weights.0148-0.8902.hdf5'
     lr = 0.00001
     num_dense = 34
-    epochs = 250
+    epochs = 1
     batch_size = 128
     dropout_rate = 0.
     trainable = True
+    res_thresh = .5
     model = heartnet_transfer(load_path=load_path,lr=lr,num_dense=num_dense,trainable=trainable,dropout_rate=dropout_rate)
     plot_model(model,"model.png",show_layer_names=True,show_shapes=True)
 
-    ##### Load Data ######
+    ###### Load Data ######
 
     feat = tables.open_file(fold_dir + foldname + '.mat')
     x_train = feat.root.trainX[:]
@@ -66,7 +112,7 @@ if __name__ == '__main__':
     train_parts = feat.root.train_parts[:]
     val_parts = feat.root.val_parts[0, :]
 
-    ################### Reshaping ############
+    ############### Reshaping ############
 
     x_train, y_train, x_val, y_val = reshape_folds(x_train, x_val, y_train, y_val)
     y_train = to_categorical(y_train,num_classes=3)
@@ -91,10 +137,13 @@ if __name__ == '__main__':
               epochs=epochs,
               verbose=2,
               shuffle=True,
-              callbacks=[modelcheckpnt, tensbd, csv_logger],
+              callbacks=[modelcheckpnt,
+                         log_UAR(x_val, y_val, val_parts),
+                         tensbd, csv_logger],
               validation_data=(x_val,y_val))
 
-    res_thresh =.5
+    ###### Results #####
+
     y_pred = model.predict(x_val, verbose=0)
     y_pred = np.argmax(y_pred,axis=-1)
     y_val = np.transpose(np.argmax(y_val,axis=-1))
@@ -107,17 +156,22 @@ if __name__ == '__main__':
             continue
         # ~ print "part {} start {} stop {}".format(s,start_idx,start_idx+int(s)-1)
 
-        temp_ = np.mean(y_val[start_idx:start_idx + int(s) - 1])
-        temp = np.mean(y_pred[start_idx:start_idx + int(s) - 1])
+        temp_ = y_val[start_idx:start_idx + int(s) - 1]
+        temp = y_pred[start_idx:start_idx + int(s) - 1]
 
-        if temp > res_thresh:
-            pred.append(1)
-        else:
+        if (sum(temp==0) > sum(temp==1)) and  (sum(temp==0) > sum(temp==2)):
             pred.append(0)
-        if temp_ > res_thresh:
-            true.append(1)
+        elif (sum(temp==2) > sum(temp==1)) and  (sum(temp==2) > sum(temp==0)):
+            pred.append(2)
         else:
+            pred.append(1)
+
+        if (sum(temp_ == 0) > sum(temp_ == 1)) and (sum(temp_ == 0) > sum(temp_ == 2)):
             true.append(0)
+        elif (sum(temp_ == 2) > sum(temp_ == 1)) and (sum(temp_ == 2) > sum(temp_ == 0)):
+            true.append(2)
+        else:
+            true.append(1)
 
         start_idx = start_idx + int(s)
     score = recall_score(y_pred=pred, y_true=true, average='macro')
